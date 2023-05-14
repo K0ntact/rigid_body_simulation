f284ef06a0e3f4027bae3ec978716164905f5e1b
Kontact <duyan5745@gmail.com>
2023-05-14 21:54:01 +0700

    refractor: move particle2d functions implementation to particle2d.cpp

--- src/header/particle2d.hpp
+++ src/header/particle2d.hpp
@@ -1,4 +1,4 @@
-#include "vector2.h"
+#include "vector2.hpp"
 
 class Particle2D {
 private:
@@ -6,39 +6,17 @@ private:
     Vector2 velocity;
     Vector2 acceleration;
     float damping;  // Percentage of velocity remains after each second. 0 is full drag, 1 is no drag.
-    float forceAccum = { 0 };
-    float inverseMass = { 0 }; // Allows for infinite mass (immovable objects)
+    float forceAccum;
+    float inverseMass; // Allows for infinite mass when = 0 (immovable objects)
 
 public:
-    void setInverseMass(float invMass) {
-        this->inverseMass = invMass;
-    }
-    void setMass(float mass) {
-        this->inverseMass = 1 / mass;
-    }
-    void setVelocity(float x, float y) {
-        this->velocity = { x,y };
-    }
-    void setAcceleration(float x, float y) {
-        this->acceleration = { x, y };
-    }
-    void setDamping(float damp) {
-        this->damping = damp;
-    }
-
-    void update(float time) {
-        assert(time > 0.0);
-        // Update position
-        // x' = x + v*t + 1/2*a*t^2
-        // x += (v*t + 1/2*a*t^2)
-        this->position.addScaledVector(this->velocity, time);
-        this->position.addScaledVector(this->acceleration, 0.5f * time * time); // Very small difference in position, consider removing
-
-        // Update velocity
-        // v = v0*damping + a*t
-        // damping = damping^t (so that reduction of velocity is proportional to time)
-        this->velocity *= powf(damping, time);
-        this->velocity.addScaledVector(this->acceleration, time);
-    }
+    Particle2D();
+    Particle2D(Vector2 pos, Vector2 vel, Vector2 acc, float damp, float invMass);
+    void setInverseMass(float invMass);
+    void setMass(float mass);
+    void setVelocity(float x, float y);
+    void setAcceleration(float x, float y);
+    void setDamping(float damp);
+    void update(float time);
 };
 
---
+++ src/particle2d.cpp
@@ -0,0 +1,56 @@
+#include "header//particle2d.hpp"
+
+Particle2D::Particle2D() {
+    this->position = {0, 0};
+    this->velocity = {0, 0};
+    this->acceleration = {0, 0};
+    this->damping = {0};
+    this->forceAccum = {0};
+    this->inverseMass = {0};
+}
+
+Particle2D::Particle2D(Vector2 pos, Vector2 vel, Vector2 acc, float damp, float invMass) {
+    this->position = pos;
+    this->velocity = vel;
+    this->acceleration = acc;
+    this->damping = damp;
+    this->forceAccum = {0};
+    this->inverseMass = invMass;
+}
+
+void Particle2D::setInverseMass(float invMass) {
+    this->inverseMass = invMass;
+}
+
+void Particle2D::setMass(float mass) {
+    this->inverseMass = 1 / mass;
+}
+
+void Particle2D::setVelocity(float x, float y) {
+    this->velocity = { x,y };
+}
+
+void Particle2D::setAcceleration(float x, float y) {
+    this->acceleration = { x, y };
+}
+
+void Particle2D::setDamping(float damp) {
+    this->damping = damp;
+}
+
+void Particle2D::update(float time) {
+    assert(time > 0.0);
+    // Update position
+    // x' = x + v*t + 1/2*a*t^2
+    // x += (v*t + 1/2*a*t^2)
+    this->position.addScaledVector(this->velocity, time);
+    this->position.addScaledVector(this->acceleration, 0.5f * time * time); // Very small difference in position, consider removing
+
+    // Update velocity
+    // v = v0*damping + a*t
+    // damping = damping^t (so that reduction of velocity is proportional to time)
+    this->velocity *= powf(damping, time);
+    this->velocity.addScaledVector(this->acceleration, time);
+}
+
+
