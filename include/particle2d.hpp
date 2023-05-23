ac3aa4011689969dafd3a815d302a30722837d92
Kontact <duyan5745@gmail.com>
2023-05-23 14:15:04 +0700

    refractor(particle2d): make forceAccum a 2D vector

--- include/particle2d.hpp
+++ include/particle2d.hpp
@@ -9,7 +9,7 @@ protected:
     Vector2 velocity;
     Vector2 acceleration;
     float damping;  // Percentage of velocity remains after each second. 0 is full drag, 1 is no drag.
-    float forceAccum;
+    Vector2 forceAccum; // Accumulated force to be applied to the particle
     float inverseMass; // Allows for infinite mass when = 0 (immovable objects)
 
 public:
@@ -21,6 +21,7 @@ public:
     void setAcceleration(float x, float y);
     void setDamping(float damp);
     virtual void update(float time);
+    void clearAccum();
 };
 
 #endif //PARTICLE2D_HPP
--- src/particle2d.cpp
+++ src/particle2d.cpp
@@ -5,7 +5,7 @@ Particle2D::Particle2D() {
     this->velocity = {0, 0};
     this->acceleration = {0, 0};
     this->damping = {0};
-    this->forceAccum = {0};
+    this->forceAccum = {0, 0};
     this->inverseMass = {0};
 }
 
@@ -51,6 +51,10 @@ void Particle2D::update(float time) {
     // damping = damping^t (so that reduction of velocity is proportional to time)
     this->velocity *= powf(damping, time);
     this->velocity.addScaledVector(this->acceleration, time);
+}
+
+void Particle2D::clearAccum() {
+    forceAccum = {0, 0};
 }
 
 
