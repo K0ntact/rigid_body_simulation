76a7c3b4ae1335626c9a2ec3ca511aabe7921611
Kontact <duyan5745@gmail.com>
2023-05-23 13:51:56 +0700

    feat(main): frame rate cap

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 
 #include "game.hpp"
 
+int FPS = 60;
+int DELAY_TIME = 1000 / FPS;
+
 Game* g_game = nullptr;
 
 int main(int argc, char* argv[]) {
@@ -16,10 +19,17 @@ int main(int argc, char* argv[]) {
     Game::Instance()->init("Chapter 1", 100, 100, 640, 480, true);
 
     while(Game::Instance()->running()) {
+        unsigned int frameStart = SDL_GetTicks();
+
         Game::Instance()->handleEvents();
-        Game::Instance()->update();
+        Game::Instance()->update(DELAY_TIME);
         Game::Instance()->render();
-        SDL_Delay(10);
+
+        unsigned int frameTime = frameStart - SDL_GetTicks();
+        if (frameTime < DELAY_TIME) {   // To cap frame rate = FPS
+            unsigned int frameDelay = DELAY_TIME - frameTime;
+            SDL_Delay(frameDelay);
+        }
     }
     Game::Instance()->clean();
 
